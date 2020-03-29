dffe4757e47d6777f9b1902a5c2adccce6309b0c
Stacey <Stacey@MacBook-Pro-3.local>
2020-03-29 13:18:33 -0700

    Added upstream

--- server/index.js
+++ server/index.js
@@ -20,6 +20,11 @@ app.all("/api/shoes/*", function(req, res) {
     apiProxy.web(req, res, {target: serverTwo});
 });
 
+app.all("/api/shoe/*", function(req, res) {
+    console.log('redirecting to Server2');
+    apiProxy.web(req, res, {target: serverTwo});
+});
+
 app.all("/api/products/*", function(req, res) {
     console.log('redirecting to Server3');
     apiProxy.web(req, res, {target: serverThree}, (err) => {
